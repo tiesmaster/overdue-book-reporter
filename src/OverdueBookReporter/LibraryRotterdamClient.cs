8d70e754da794062f882fcc6a1c98e2a86b7407e
Thijs Brobbel <thijsbrobbel@gmail.com>
2023-10-15 22:15:24 +0200

    Remove the explicit setting of certain values

--- src/OverdueBookReporter/LibraryRotterdamClient.cs
+++ src/OverdueBookReporter/LibraryRotterdamClient.cs
@@ -20,21 +20,10 @@ public class LibraryRotterdamClient
 
     private static HttpClient CreateLibraryRotterdamClient()
     {
-        var client = new HttpClient(new HttpClientHandler
+        return new HttpClient(new HttpClientHandler
         {
             AllowAutoRedirect = false
         });
-
-        client.DefaultRequestHeaders.Add(
-            "User-Agent",
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/113.0");
-
-        client.DefaultRequestHeaders.Add(
-            "Accept",
-            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
-        );
-
-        return client;
     }
 
     public async Task StartSessionAsync()
