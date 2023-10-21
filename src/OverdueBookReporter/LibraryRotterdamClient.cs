0fba9992cdc02ca58b082705efad264457c0c2ab
Thijs Brobbel <thijsbrobbel@gmail.com>
2023-10-21 21:40:53 +0200

    Simplify some stuff in the library rotterdam client

--- src/OverdueBookReporter/LibraryRotterdamClient.cs
+++ src/OverdueBookReporter/LibraryRotterdamClient.cs
@@ -1,22 +1,17 @@
 using System.Text;
 
-using AngleSharp.Io;
-
 using Microsoft.Extensions.Options;
 
 namespace Tiesmaster.OverdueBookReporter;
 
 public class LibraryRotterdamClient
 {
-    public const string HttpClientName = nameof(LibraryRotterdamClient);
-
     private readonly LibraryLoginCredentials _credentials;
     private readonly HttpClient _httpClient;
     private readonly CookieJar _cookieJar;
     private readonly ILogger<LibraryRotterdamClient> _logger;
 
     private bool _isLoggedIn;
-    private LoginPageResult? _loginFormSecurityValues;
 
     public LibraryRotterdamClient(
         HttpClient httpClient,
@@ -28,55 +23,6 @@ public class LibraryRotterdamClient
         _httpClient = httpClient;
         _cookieJar = cookieJar;
         _logger = logger;
-    }
-
-    public async Task StartSessionAsync()
-    {
-        _logger.LogDebug("Starting session");
-        var response = await _httpClient.GetAsync("https://www.bibliotheek.rotterdam.nl/login");
-
-        _cookieJar.ReadCookieValues(response);
-
-        var html = await response.Content.ReadAsStringAsync();
-        var result = await LibraryHtmlParser.ParseLoginPageAsync(html);
-        _loginFormSecurityValues = result;
-
-        _logger.LogTrace("Login page security values: {LoginFormSecurityValues}", _loginFormSecurityValues);
-    }
-
-    public async Task LoginAsync()
-    {
-        await StartSessionAsync();
-
-        _logger.LogDebug("Logging in");
-        var dict = new Dictionary<string, string>
-        {
-            { "username", _credentials.Username },
-            { "password", _credentials.Password },
-            { "return", _loginFormSecurityValues!.ReturnToken },
-            { _loginFormSecurityValues!.CsrfToken, "1" },
-        };
-        var body = new FormUrlEncodedContent(dict);
-        var ms = new MemoryStream();
-        await body.CopyToAsync(ms);
-        _cookieJar.AddCookies(body.Headers);
-
-        _logger.LogTrace("Form content: {FormContent}", Encoding.UTF8.GetString(ms.ToArray()));
-
-        var response = await _httpClient.PostAsync("https://www.bibliotheek.rotterdam.nl/login?task=user.login", body);
-
-        _logger.LogTrace("Received status code: {StatusCode}", response.StatusCode);
-        _logger.LogTrace("Received HTML: {Html}", await response.Content.ReadAsStringAsync());
-
-        _logger.LogTrace("Response headers: {ResponseHeaders}", FlattenHeaderValues(response.Headers));
-        _logger.LogTrace("Content headers: {ContentHeaders}", FlattenHeaderValues(response.Content.Headers));
-
-        _cookieJar.ReadCookieValues(response);
-        _cookieJar.AddCookies(_httpClient.DefaultRequestHeaders);
-
-        // TODO: Should read this from Location: redirect header
-        var response2 = await _httpClient.GetAsync("https://www.bibliotheek.rotterdam.nl/mijn-menu");
-        _cookieJar.ReadCookieValues(response2);
     }
 
     public async Task<IEnumerable<LoanedBook>> GetBookListingAsync()
@@ -99,6 +45,56 @@ public class LibraryRotterdamClient
         _logger.LogTrace("Received HTML: {Html}", content);
 
         return await LibraryHtmlParser.ParseBookListingAsync(content);
+    }
+
+    private async Task LoginAsync()
+    {
+        var loginFormSecurityValues = await StartSessionAsync();
+
+        _logger.LogDebug("Logging in");
+        var dict = new Dictionary<string, string>
+        {
+            { "username", _credentials.Username },
+            { "password", _credentials.Password },
+            { "return", loginFormSecurityValues!.ReturnToken },
+            { loginFormSecurityValues!.CsrfToken, "1" },
+        };
+
+        var body = new FormUrlEncodedContent(dict);
+        var ms = new MemoryStream();
+        await body.CopyToAsync(ms);
+        _cookieJar.AddCookies(body.Headers);
+
+        _logger.LogTrace("Form content: {FormContent}", Encoding.UTF8.GetString(ms.ToArray()));
+
+        var loginResponse = await _httpClient.PostAsync("https://www.bibliotheek.rotterdam.nl/login?task=user.login", body);
+
+        _logger.LogTrace("Received status code: {StatusCode}", loginResponse.StatusCode);
+        _logger.LogTrace("Received HTML: {Html}", await loginResponse.Content.ReadAsStringAsync());
+
+        _logger.LogTrace("Response headers: {ResponseHeaders}", FlattenHeaderValues(loginResponse.Headers));
+        _logger.LogTrace("Content headers: {ContentHeaders}", FlattenHeaderValues(loginResponse.Content.Headers));
+
+        _cookieJar.ReadCookieValues(loginResponse);
+        _cookieJar.AddCookies(_httpClient.DefaultRequestHeaders);
+
+        var redirectResponse = await _httpClient.GetAsync(loginResponse.Headers.Location);
+        _cookieJar.ReadCookieValues(redirectResponse);
+    }
+
+    private async Task<LoginPageResult> StartSessionAsync()
+    {
+        _logger.LogDebug("Starting session");
+        var response = await _httpClient.GetAsync("https://www.bibliotheek.rotterdam.nl/login");
+
+        _cookieJar.ReadCookieValues(response);
+
+        var html = await response.Content.ReadAsStringAsync();
+        var result = await LibraryHtmlParser.ParseLoginPageAsync(html);
+
+        _logger.LogTrace("Login page security values: {LoginFormSecurityValues}", result);
+
+        return result;
     }
 
     private static Dictionary<string, string> FlattenHeaderValues(
