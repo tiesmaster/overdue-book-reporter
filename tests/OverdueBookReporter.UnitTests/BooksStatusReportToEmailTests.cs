96dcf3632d1f4b7c3d97848d4d6da2999ee19245
Thijs Brobbel <thijsbrobbel@gmail.com>
2023-11-14 23:37:32 +0100

    Validate culture

--- tests/OverdueBookReporter.UnitTests/BooksStatusReportToEmailTests.cs
+++ tests/OverdueBookReporter.UnitTests/BooksStatusReportToEmailTests.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace Tiesmaster.OverdueBookReporter.UnitTests;
 
 public class BooksStatusReportToEmailTests
@@ -127,6 +129,7 @@ public class BooksStatusReportToEmailTests
             var emailBody = report.GetBody();
 
             // assert
+            CultureInfo.CurrentCulture.Should().Be(CultureInfo.GetCultureInfo("nl-NL"));
             emailBody.Should().Be("""
                 ALL books due: 10 days from now
 
