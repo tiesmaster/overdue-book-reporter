be77c9e122fabffa71b10d74c92f632419cba27d
Thijs Brobbel <thijsbrobbel@gmail.com>
2024-10-01 20:54:48 +0200

    WIP

--- src/OverdueBookReporter.AppHost/Program.cs
+++ src/OverdueBookReporter.AppHost/Program.cs
@@ -1,3 +1,15 @@
+using k8s.Models;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+builder
+    .AddContainer("smtp4dev", "rnwood/smtp4dev")
+    .WithHttpEndpoint(port: 2525, targetPort: 80)
+    .WithHttpEndpoint(port: 25, targetPort: 25);
+    //.WithEndpoint(2525, 80)
+    //.WithEndpoint(25, 25);
+
+builder
+    .AddProject<Projects.OverdueBookReporter>("main-reporter-job");
 
 builder.Build().Run();
